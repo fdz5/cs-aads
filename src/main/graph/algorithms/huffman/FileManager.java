e2d4a04c5651ef156e87b7c8ecee8ad87aa12b59
Filip Dziedzic <filipdz@mops.uci.agh.edu.pl>
2014-01-13 09:58:40 +0100

    Add bits wirte and read

--- src/main/graph/algorithms/huffman/FileManager.java
+++ src/main/graph/algorithms/huffman/FileManager.java
@@ -1,6 +1,16 @@
 package main.graph.algorithms.huffman;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.BitSet;
 import java.util.HashMap;
 
@@ -29,6 +39,22 @@ public class FileManager {
         br.close();
         writer.close();
     }
+    
+    public static void encodeBits(HashMap<Character, String> codeMap, String from, String to) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(from)));
+        FileOutputStream writer = new FileOutputStream(to);
+        char c;
+        int i;
+        StringBuilder sb = new StringBuilder();
+        while ((i = br.read()) != -1) {
+            c = (char) i;
+            sb.append(codeMap.get(c));
+        }
+        BitSet bs = fromString(sb.toString());
+        writer.write(toByteArray(bs));
+        br.close();
+        writer.close();
+    }
 
     public static void decode(HashMap<String, Character> codeMap, String from, String to) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(from)));
@@ -47,9 +73,37 @@ public class FileManager {
         br.close();
         writer.close();
     }
+    
+    public static void decodeBits(HashMap<String, Character> codeMap, String from, String to) throws IOException {
+        PrintWriter writer = new PrintWriter(to, "UTF-8");
+       
+        Path path = Paths.get(from);
+        byte[] fileData = Files.readAllBytes(path);
+        
+        
+        StringBuilder all = new StringBuilder();
+        
+        for (byte b : fileData) {
+        	String tmp = byte2Str(b);
+//        	System.out.println(tmp);
+        	all.append(tmp);
+        }
+//        System.out.println(all.toString());
+        
+        String temp = "";
+        for (char c : all.toString().toCharArray()) {
+        	temp = temp.concat(String.valueOf(c));
+            if (codeMap.containsKey(temp)) {
+                writer.print(codeMap.get(temp));
+                temp = "";
+            }
+        }
 
-    private byte[] toByteArray(BitSet bits) {
-        byte[] bytes = new byte[(bits.length() + 7) / 8];
+        writer.close();
+    }
+
+    public static byte[] toByteArray(BitSet bits) {
+        byte[] bytes = new byte[bits.length()/8+1];
         for (int i=0; i<bits.length(); i++) {
             if (bits.get(i)) {
                 bytes[bytes.length-i/8-1] |= 1<<(i%8);
@@ -58,11 +112,49 @@ public class FileManager {
         return bytes;
     }
 
-    private BitSet fromString(final String s) {
-        return BitSet.valueOf(new long[] { Long.parseLong(s, 2) });
+//    private static BitSet fromString(final String s) {
+//        return BitSet.valueOf(new long[] { Long.parseLong(s, 2) });
+//    }
+
+    private static BitSet fromString(String s) {
+    	BitSet bs = new BitSet();
+    	int i =0;
+    	for (char c: s.toCharArray()) {
+//    		System.out.println(c);
+    		if (c == '0') {
+    			bs.clear(i);
+    		}
+    		if (c == '1') {
+    			bs.set(i);
+    		}
+    		i++;    			
+    	}
+//    	System.out.println(i);
+//    	System.out.println(bs.length());
+    	return bs;
     }
 
-    private String toString(BitSet bs) {
+    private static String byte2Str(byte b) {
+    	StringBuilder sb = new StringBuilder();
+    	byte c = b;
+    	int[] temp = new int[8];
+    	for (int j = 0; j<8; j++) {
+//    		System.out.println(c);
+        	int x  = c & 1;
+        	temp[j] = x;
+        	c = (byte)(c >> 1);
+    	}
+    	for (int i =7; i>=0; i--) {
+//    		System.out.println(temp[i]);
+    		if (temp[i] == 1)
+    			sb.append("1");
+    		else 
+    			sb.append("0");
+    	}
+    	return sb.toString();
+    }
+    
+    private static String toString(BitSet bs) {
         return Long.toString(bs.toLongArray()[0], 2);
     }
 
--- src/test/algorithms/huffman/HuffmanTest.java
+++ src/test/algorithms/huffman/HuffmanTest.java
@@ -1,51 +1,93 @@
 package test.algorithms.huffman;
 
-import main.graph.algorithms.huffman.HuffmanTree;
-import main.graph.algorithms.huffman.FileManager;
-import main.graph.algorithms.huffman.Histogram;
-import main.graph.algorithms.huffman.Huffman;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+
+import main.graph.algorithms.huffman.FileManager;
+import main.graph.algorithms.huffman.Histogram;
+import main.graph.algorithms.huffman.Huffman;
+import main.graph.algorithms.huffman.HuffmanTree;
+
+import org.junit.Test;
 
 /**
- * User: filip
- * Date: 12.01.14
- * Time: 19:21
+ * User: filip Date: 12.01.14 Time: 19:21
  */
 public class HuffmanTest {
 
-    public static void main(String[] args) throws IOException {
+	@Test
+	public void histogramTest() throws IOException {
+		Histogram h = new Histogram();
+		FileManager.read(h, "big.txt");
+		h.sort();
+		TreeMap<Character, Integer> tm = h.getHistogram();
+		System.out.println("Tree map size: " + tm.size());
+		System.out.println("Base size: " + h.getBase().size());
+		h.printHistogram();
+	}
 
-        Histogram h = new Histogram();
-        FileManager.read(h, "big.txt");
-        h.sort();
-        Huffman huffman = new Huffman(h);
+	@Test
+	public void encodeTest() throws IOException {
+		Histogram h = new Histogram();
+		FileManager.read(h, "big.txt");
+		h.sort();
+		Huffman huffman = new Huffman(h);
 
-//        TreeMap<Character, Integer> tm = h.getHistogram();
-//        System.out.println("Tree map size: " + tm.size());
-//        System.out.println("Base size: " + h.getBase().size());
-//        h.printHistogram();
+		HuffmanTree huffmanTree = huffman.getHuffmanTree();
+		huffman.computeCodeMaps(huffmanTree);
+		huffmanTree.printTree();
+		HashMap<Character, String> codeMap = huffman.getCodeMap();
+		FileManager.encodeBits(codeMap, "big.txt", "encoded.txt");
+	}
 
-        HuffmanTree huffmanTree = huffman.getHuffmanTree();
-//        huffmanTree.printTree();
-        huffman.computeCodeMaps(huffmanTree);
+	@Test
+	public void decodeTest() throws IOException {
+		Histogram h = new Histogram();
+		FileManager.read(h, "big.txt");
+		h.sort();
+		Huffman huffman = new Huffman(h);
 
-        HashMap<Character, String> codeMap = huffman.getCodeMap();
-        HashMap<String, Character> decodeMap = huffman.getDecodeMap();
+		HuffmanTree huffmanTree = huffman.getHuffmanTree();
+		huffman.computeCodeMaps(huffmanTree);
+		HashMap<Character, String> codeMap = huffman.getCodeMap();
+		HashMap<String, Character> decodeMap = huffman.getDecodeMap();
 
-        System.out.println("Encode map");
-        for (Map.Entry<Character, String> entry : codeMap.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
-        }
-        System.out.println("Decode map");
-        for (Map.Entry<String, Character> entry : decodeMap.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
-        }
+		FileManager.encodeBits(codeMap, "big.txt", "encoded.txt");
+		FileManager.decodeBits(decodeMap, "encoded.txt", "decoded.txt");
+	}
 
-        FileManager.encode(codeMap, "big.txt", "encoded.txt");
-        FileManager.decode(decodeMap, "encoded.txt", "decoded.txt");
-    }
+	// public static void main(String[] args) throws IOException {
+	//
+	// Histogram h = new Histogram();
+	// FileManager.read(h, "big.txt");
+	// h.sort();
+	// Huffman huffman = new Huffman(h);
+	//
+	// // TreeMap<Character, Integer> tm = h.getHistogram();
+	// // System.out.println("Tree map size: " + tm.size());
+	// // System.out.println("Base size: " + h.getBase().size());
+	// // h.printHistogram();
+	//
+	// HuffmanTree huffmanTree = huffman.getHuffmanTree();
+	// // huffmanTree.printTree();
+	// huffman.computeCodeMaps(huffmanTree);
+	//
+	// HashMap<Character, String> codeMap = huffman.getCodeMap();
+	// HashMap<String, Character> decodeMap = huffman.getDecodeMap();
+	//
+	// System.out.println("Encode map");
+	// for (Map.Entry<Character, String> entry : codeMap.entrySet()) {
+	// System.out.println(entry.getKey() + " - " + entry.getValue());
+	// }
+	// System.out.println("Decode map");
+	// for (Map.Entry<String, Character> entry : decodeMap.entrySet()) {
+	// System.out.println(entry.getKey() + " - " + entry.getValue());
+	// }
+	//
+	//
+	//
+	// }
 
 }
